6b99bd55b6d55bd13815a123466bc35732eef42e
Nimish David Mathew <Nimish David Mathew>
2017-01-08 22:11:45 +0530

    Working login

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'ndm0150040'
+    },
+	'database': 'college',
+    'users_table': 'users'
+};
